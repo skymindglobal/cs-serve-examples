fc913c5c8d7249663704cda12b3882a0be7fb7c3
MZulfadzli <mohdzulfadzli3@gmail.com>
2021-08-23 14:41:45 +0800

    updating repository

---
+++ Face Detection/scripts/send-video/send-video.py
@@ -0,0 +1,51 @@
+import cv2
+import requests
+import time
+import sys
+
+# Create a VideoCapture object and read from input file
+# If the input is the camera, pass 0 instead of the video file name
+# sys.argv[1] is the device token
+# sys.argv[2] is the file path of the video
+
+cap = cv2.VideoCapture(sys.argv[2])
+
+# Uncomment to send webcam video
+# cap = cv2.VideoCapture(0)
+
+start_frame_number = 0
+successive_frames_to_skip = 10
+
+cap.set(cv2.CAP_PROP_POS_FRAMES, start_frame_number)
+
+# Check if camera opened successfully
+if not cap.isOpened():
+    print("Error opening video stream or file")
+
+count = 0
+# Read until video is completed
+while cap.isOpened():
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    if ret:
+        count += 1
+        if count % successive_frames_to_skip != 0:
+            continue
+
+        frame = cv2.resize(frame, (480, 320))
+        t1 = time.time()
+        cv2.imwrite("image.png", frame)
+        t2 = time.time()
+        print(f"Time: {t2 - t1}s")
+        url = f'http://cs.hyperlinx.ai/api/v1/{sys.argv[1]}/telemetry'
+        files = {'image': ('image.png', open('image.png', 'rb'), 'image/png')}
+        requests.post(url, files=files)
+        print(f"Time: {time.time() - t2}s")
+    else:
+        break
+
+# When everything done, release the video capture object
+cap.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()
---
+++ Object Classification/scripts/send image/send_folder_image.py
@@ -0,0 +1,30 @@
+# Import Module
+import os
+import time
+
+import requests
+
+# Folder Path
+path = $IMAGE_FOLDER #replace $IMAGE_FOLDER with the directory of the image folder
+
+# Change the directory
+os.chdir(path)
+
+url = 'http://$URL_DOMAIN/api/v1/$ACCESS_TOKEN/telemetry' #replace $ACCESS_TOKEN with input access token && $URL_DOMAIN with url domain
+
+# iterate through all image files
+for imagefile in os.listdir():
+    # Check whether file is in which format
+    if imagefile.endswith(".jpg"):
+        print(imagefile)
+        files = {'image': (imagefile,open(imagefile,'rb'), 'image/jpg')}
+        requests.post(url, files=files)
+        print("Send " + imagefile + " success")
+    elif imagefile.endswith(".png"):
+        print(imagefile)
+        files = {'image': (imagefile,open(imagefile,'rb'), 'image/png')}
+        requests.post(url, files=files)
+        print("Send " + imagefile + " success")
+    time.sleep(3)
+
+print("Send Completed")
