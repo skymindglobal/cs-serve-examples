887b491eeee9cf65a64ae6f2324cef91a699e4f6
Mohd Zulfadzli <73336970+MZulfadzli@users.noreply.github.com>
2021-08-27 16:49:50 +0800

    adding comments to python script

--- Iris Classification/scripts/send-data/sendIrisRandomData.py
+++ Iris Classification/scripts/send-data/sendIrisRandomData.py
@@ -1,24 +1,27 @@
-import pandas as pd
-import numpy as np
-from random import randint
-import requests
-import time
-from datetime import datetime
+import pandas as pd                      # pandas library to read CSV file
+import numpy as np                       # numpy library to handle array data
+from random import randint               # generate random
+import requests                          # requests allows you to send HTTP/1.1 requests
+import time                              # use for delay if needed
+import sys                               # to take the arguments on terminal
+from datetime import datetime            # use for current sending time
+
+# sys.argv[1] is the Input access token
 
 
 def main():
-    url = 'http://board.konsole.ai/api/v1/$ACCESS_TOKEN/telemetry'  # replace $ACCESS_TOKEN with input access token
-    irisdata = pd.read_csv("./../../data/Iris.csv", header=0, index_col='Id')
-
+    url = f'https://cs.hyperlinx.ai/api/v1/{sys.argv[1]}/telemetry'             # URL of CS.Serve
+    irisdata = pd.read_csv("./../../data/Iris.csv", header=0, index_col='Id')   # read data from source directory
+    # Generate random and post the data until completed
     for i in range(10):
-        v = randint(0, irisdata.shape[0] - 1)
+        v = randint(0, irisdata.shape[0] - 1)                                                                       # generate random
         sepallen, sepalwid, petallen, petalwid = irisdata.iloc[v][0], irisdata.iloc[v][1], irisdata.iloc[v][2], \
-                                                 irisdata.iloc[v][3]
-        inputdata = np.array([[sepallen, sepalwid, petallen, petalwid]])
+                                                 irisdata.iloc[v][3]                                                # assigned input features
+        inputdata = np.array([[sepallen, sepalwid, petallen, petalwid]])                                            # put data into array
         # print(inputdata.tolist())
-        print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"), "Sending random data of", irisdata.iloc[v][4])
-        requests.post(url, files={'input': (None, str(inputdata.tolist()))})  # '[[5.1,3.5,1.4,0.2]]'
-        time.sleep(0.5)
+        print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"), "Sending random data of", irisdata.iloc[v][4])          # print post time
+        requests.post(url, files={'input': (None, str(inputdata.tolist()))})                                        # post image to URL
+        time.sleep(0.5)                                                                                             # delay time
 
     print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"), "Completed")
 
@@ -26,4 +29,4 @@ def main():
 if __name__ == '__main__':
     main()
 
-#  curl -v -X POST http://board.konsole.ai/api/v1/$ACCESS_TOKEN/telemetry -F input=[[5.1,3.5,1.4,0.2]]
+#  curl -v -X POST https://cs.hyperlinx.ai/api/v1/$ACCESS_TOKEN/telemetry -F input=[[5.1,3.5,1.4,0.2]]
