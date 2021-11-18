af8e18a158cf1a59f32591ca5b227f42567b3cc1
sitifarah-raklan <79986322+sitifarah-raklan@users.noreply.github.com>
2021-11-18 13:15:38 +0800

    Create select_bounding_box.js

---
+++ Face Recognition/scripts/select_bounding_box.js
@@ -0,0 +1,23 @@
+box = [0,0,0,0]
+score = 0;
+threshold = 0.99;
+boxes = msg.boxes[0];
+confidences = msg.scores[0];
+for (var i = 0; (i * 2) < confidences.length; i++) {
+    score_index = i * 2;
+    box_index = i * 4;
+    current_score = confidences[score_index + 1];
+    if (current_score > threshold && current_score > score) {
+        score = current_score;
+        box = [boxes[box_index], boxes[box_index + 1], boxes[box_index + 2], boxes[box_index + 3]]
+    }
+} 
+msg.bbox = {
+    "@x1": box[0],
+    "@y1": box[1],
+    "@x2": box[2],
+    "@y2": box[3],
+    "label" : "face",
+    "probability" : score
+};
+return {msg: msg, metadata: metadata, msgType: msgType};
